8b197d25d198ee0fe848a91be46c95b422176407
Paula <Paula@Ideapad530S.dubrovnik.hr>
2021-02-22 00:19:17 +0100

    weather app

--- src/index.js
+++ src/index.js
@@ -2,17 +2,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail';
-// Create a react component
-const App = () => {
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-    return (
-        <div>
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Nice Blog Post!" avatar={faker.image.image()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="Love it!" avatar={faker.image.image()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 4:45PM" comment="So inspiring..." avatar={faker.image.image()}/>
-        </div>
-    );
+// Create a react component
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null, lon: null, errorMessage: ''};
+
+        
+    }
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude, lon: position.coords.latitude})
+            },
+            err => {
+                this.setState({errorMessage: err.message})
+            }
+        );
+    }
+    componentDidUpdate(){
+        console.log('Something changed!');
+    }
+    componentWillUnmount(){
+        console.log('It dissapeared');
+    }
+    renderContent () {
+        if(this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>Error: {this.state.errorMessage}</div>
+            );
+        }
+        else if(!this.state.errorMessage && this.state.lat) {
+            return (
+                <SeasonDisplay lat={this.state.lat}/>
+            );
+        }
+        else {
+            return <Spinner message="Please accept request for location"/>;
+        }
+    }
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+        
+    }
 }
 //Take the react component and show it to screen
 ReactDOM.render(
